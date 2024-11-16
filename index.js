c1b4683e4750fe5c6108e69d040ecd28c0a2231f
KRISHNA <kraggr2909@gmail.com>
2024-11-16 23:59:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+function update() {
+    title = document.getElementById("title").value;
+    desc = document.getElementById("desc").value;
+    console.log("Updating ..... ");
+    if (localStorage.getItem('itemsJson') == null) {
+        itemsJsonArray = [];
+        itemsJsonArray.push([title, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    }
+    else {
+        let itemsJsonArraystr = localStorage.getItem('itemsJson');
+        let itemsJsonArray = JSON.parse(itemsJsonArraystr);
+        itemsJsonArray.push([title, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    }
+    loadtable();
+}
+function loadtable(){
+    if (localStorage.getItem('itemsJson') == null) {
+        itemsJsonArray = [];
+        localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    }
+    else{
+        itemsJsonArraystr = localStorage.getItem('itemsJson');
+        itemsJsonArray = JSON.parse(itemsJsonArraystr);
+    }
+    // Display through table
+    table = document.getElementById("bodyhaibhai")
+    str = "";
+    itemsJsonArray.forEach((element, index) => {
+        str += `
+            <tr>
+            <td>${index + 1}</td>
+            <td>${element[0]}</td>
+            <td>${element[1]}</td> 
+            <td><button class="btn btn-sm btn-primary" id="remove" onclick="deleting(${index})">Remove</button></td>
+            </tr>`;
+    });
+    table.innerHTML = str;
+}
+let add = document.getElementById("add");
+add.addEventListener("click", update);
+loadtable();
+//remove button
+function deleting(indexarr) {
+    let itemsJsonArraystr = localStorage.getItem('itemsJson');
+    let itemsJsonArray = JSON.parse(itemsJsonArraystr);
+
+    itemsJsonArray.splice(indexarr, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemsJsonArray));
+    loadtable();
+}
+function clearkrdo(){
+    if(confirm("Do u really want to clear storage ? ")){
+        localStorage.clear();
+        loadtable();
+    }
+}
